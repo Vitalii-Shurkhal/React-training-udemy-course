d9ca1008c1d92f37c8b00dddac69abe892624eda
Vitalii-Shurkhal <vshurkhal@gmail.com>
2022-02-07 18:33:45 +0100

    update files

--- T16_Custom-hooks-part1/src/App.js
+++ T16_Custom-hooks-part1/src/App.js
@@ -1,9 +1,10 @@
 import BasicForm from './components/BasicForm'
+import SimpleForm from './components/SimpleInput'
 
 function App() {
   return (
     <div className="app">
-      <BasicForm/>
+      <SimpleForm/>
     </div>
   );
 }
